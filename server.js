a748117d395edfd1b8ea80fb866fbf4bc9f24946
Elamathi <elamelamathi6@gmail.com>
2026-07-11 21:36:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 8800;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
